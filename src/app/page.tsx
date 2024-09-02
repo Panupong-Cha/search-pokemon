05b6d0abc822d02939c9efcc61e299e4833340b8
Panupong-Cha <panupongchareyrat@gmail.com>
2024-09-03 00:01:26 +0700

    disabling rule and add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import SearchInput from "../components/SearchInput";
 import PokemonResult from "../components/PokemonResult";
+import { Suspense } from "react";
 
 const Home = () => {
   return (
     <div className="bg-gray-100 h-screen">
       <SearchInput />
-      <PokemonResult />
+      <Suspense fallback={<p>Loading Pokémon data...</p>}>
+        <PokemonResult />
+      </Suspense>
     </div>
   );
 };
